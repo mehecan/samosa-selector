61a0298e4ba87960105d131c567eb695d97b1fff
Osvaldo Morales <osvaldom@protonmail.com>
2024-03-08 16:44:02 -0500

    created lab 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return(
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className='samosa' src='https://imgs.search.brave.com/49wq8l9iBJAUtoVNMVKqQsqpy2uLtLDShef_m1Msmzg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzQzLzg0Lzkx/LzM2MF9GXzQzODQ5/MTcwX3E0RUY3WGVS/YlBlN3M2NDBjRDBk/Qjg3aDJoTGRmZzVM/LmpwZw'/>
+      </div>
+    </div>
+  )
+}
+export default App
